dba7140d6c2f50c9d4d5ff63cf7ba7ff2b034d4d
Toniboni08 <anton.koller@schueler.fg-kassel.de>
2025-12-08 18:08:11 +0100

    kurzes design von der Forums webseite

--- forum/index.js
+++ forum/index.js
@@ -1,5 +1,5 @@
 let nameModifiable = document.querySelectorAll("#forumName");
-const searchExtensions = new URLSearchParams(document.URL.search);
+const searchExtensions = new URLSearchParams(document.location.search);
 
 for (let index = 0; index < nameModifiable.length; index++) {
     const element = nameModifiable[index];
