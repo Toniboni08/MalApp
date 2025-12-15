60f21b9b0fb395d52bd9254da2ecb882840ea1ec
Toniboni08 <anton.koller@schueler.fg-kassel.de>
2025-12-15 06:44:53 +0100

    added Test

---
+++ Test/script.js
@@ -0,0 +1,72 @@
+document.querySelector('button').onclick = () => {
+    offer()
+}
+
+let peer = new RTCPeerConnection();
+
+async function offer() {
+
+    let candidates = []
+
+    peer.onicecandidate = event => {
+        candidates.push(event.candidate);
+    }
+
+    peer.onicegatheringstatechange = event => {
+        if (peer.iceGatheringState == "complete") {
+            console.log(JSON.stringify({
+                "candidates": candidates,
+                "offer": offer
+            }));
+        }
+    }
+
+    peer.onconnectionstatechange = event => {
+        console.log(peer.connectionState);
+    }
+
+    peer.createDataChannel("asd");
+    let offer = await peer.createOffer()
+    peer.setLocalDescription(offer);
+}
+
+async function offerAnswer(answer) {
+    peer.setRemoteDescription(answer.answer);
+
+    for (let index = 0; index < answer.candidates.length; index++) {
+        const candidate = answer.candidates[index];
+        peer.addIceCandidate(candidate);
+    }
+}
+
+async function answer(offer) {
+    let candidates = []
+
+    peer.onicecandidate = event => {
+        candidates.push(event.candidate);
+    }
+
+    peer.onconnectionstatechange = event => {
+        console.log(peer.connectionState);
+    }
+
+    peer.onicegatheringstatechange = event => {
+        if (peer.iceGatheringState == "complete") {
+            console.log(JSON.stringify({
+                "candidates": candidates,
+                "answer": answer
+            }));
+        }
+    }
+
+
+    peer.setRemoteDescription(offer.offer);
+
+    for (let index = 0; index < offer.candidates.length; index++) {
+        const candidate = offer.candidates[index];
+        peer.addIceCandidate(candidate);
+    }
+
+    let answer = await peer.createAnswer()
+    peer.setLocalDescription(answer);
+}
